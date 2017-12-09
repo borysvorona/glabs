d61eb40dd3ccfc1bb13f5801c2e0551d7476840a
Borys Vorona <borysvorona@gmail.com>
2017-12-09 22:51:22 +0200

    added ordinary least squares class

--- lab1/utils.py
+++ lab1/utils.py
@@ -1,19 +1,86 @@
-from math import *
+from sympy import N, symbols, sympify, zoo
 
 
-def tg(x):  # real signature unknown;
-    """
-    ctg(x)
+class Function(object):
+    #   1+ctg(x/2)-(1-2*cos(x/2)**2)+sin(x)**2
+    #   1+cot(x/2)-(1-2*cos(x/2)**2)+sin(x)**2
+    #   1+(cos(x/2)/sin(x/2))-(1-2*cos(x/2)**2)+sin(x)**2
 
-    Return the tangens of x.
-    """
-    return tan(x)
+    def __init__(self, expr, start, end, step):
+        self.expr = expr
+        self.start = start
+        self.end = end
+        self.step = step
+
+    @property
+    def get_result(self):
+        x = symbols('x')
+        xx = self.start
+        result = []
+        while xx <= self.end:
+            xx += self.step
+            try:
+                result.append([float(xx),
+                               (None if N(sympify(self.expr).subs(x, xx)) == zoo else N(sympify(self.expr).subs(x, xx)))])
+            except Exception as ex:
+                print(ex)
+        return result
+
+    @property
+    def get_ordinate_only(self):
+        x = symbols('x')
+        xx = self.start
+        result = []
+        while xx <= self.end:
+            xx += self.step
+            try:
+                result.append(None if N(sympify(self.expr).subs(x, xx)) == zoo else N(sympify(self.expr).subs(x, xx)))
+            except Exception as ex:
+                print(ex)
+        return result
+
+    @property
+    def get_abscissa_only(self):
+        xx = self.start
+        result = []
+        while xx <= self.end:
+            xx += self.step
+            result.append(float(xx))
+        return result
+
+    @property
+    def count(self):
+        xx = self.start
+        count = 0
+        while xx <= self.end:
+            xx += self.step
+            count += 1
+        return count
 
 
-def ctg(x):  # real signature unknown;
-    """
-    ctg(x)
+class OrdinaryLeastSquares(object):
 
-    Return the complementi tangens of x.
-    """
-    return cos(x) / sin(x)
+    def __init__(self, func):
+        self.func = func
+        self.n = self.func.count
+        self.sum_x = sum(self.func.get_abscissa_only)
+        self.sum_y = sum(self.func.get_ordinate_only)
+        self.sum_xx = sum([i * i for i in self.func.get_abscissa_only])
+        self.sum_xy = sum([i[0]*i[1] for i in self.func.get_result])
+
+    @property
+    def get_a(self):
+        return (self.n * self.sum_xy - self.sum_x * self.sum_y) / (self.n * self.sum_xx - self.sum_x**2)
+
+    @property
+    def get_b(self):
+        return (self.sum_y - self.get_a * self.sum_x) / self.n
+
+    @property
+    def get_expression(self):
+        print(self.n)
+        print(self.sum_x)
+        print(self.sum_y)
+        print(self.sum_xx)
+        print(self.sum_xy)
+        return '%s * x + %s' % (self.get_a, self.get_b)
--- lab1/views.py
+++ lab1/views.py
@@ -1,28 +1,20 @@
 from django.shortcuts import render
-from .utils import *
 
+from .utils import Function, OrdinaryLeastSquares
 from .forms import MathExpressionForm
 
 
 def home_index(request):
     form = MathExpressionForm(request.POST or None)
+    firstf = secondf = thirdf = None
     if form.is_valid():
         data = form.cleaned_data
-        first = get_chart_points(data['expression'], data['start'], data['end'], data['step'])
-        print(first)
+        firstf = Function(data['expression'], data['start'], data['end'], data['step'])
+        print(firstf.get_result)
+        secondf = OrdinaryLeastSquares(firstf).get_expression
+        print(secondf)
     return render(request,
-                  'lab1/home_index.html', {'form': form})
-
-
-def get_chart_points(expr, start, end, step):
-    #   1+ctg(x/2)-(1-2*cos(x/2)**2)+sin(x)**2
-    x = start
-    result = []
-    while x < end:
-        try:
-            result.append([float(x), eval(expr)])
-        except ZeroDivisionError:
-            print('Gap at point %s. Solution does not exist:' % x)
-            result.append([float(x), None])
-        x += step
-    return result
+                  'lab1/home_index.html', {'form': form,
+                                           'first': firstf.get_result,
+                                           'second': secondf,
+                                           'third': thirdf})
