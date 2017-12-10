0395c14fd3b00c9d8ced61c97c681790299252f7
Borys Vorona <borysvorona@gmail.com>
2017-12-10 22:10:41 +0200

    fixed html

--- lab1/forms.py
+++ lab1/forms.py
@@ -6,8 +6,8 @@ from lab1.models import MathExpression
 class MathExpressionForm(ModelForm):
     class Meta:
         model = MathExpression
-        fields = ['start', 'end', 'expression']
-        exclude = ['step']
+        fields = ['expression', 'start', 'end',  'step']
+        # exclude = ['step']
 
     def clean(self):
         cleaned_data = self.cleaned_data
